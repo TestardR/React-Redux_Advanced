d5dd9b9bb62c4a57f33159cb11ee4b54c841a5b5
unknown <rmtestard@gmail.com>
2019-04-04 17:12:31 +0200

    testing with enzyme and absolute path implementation

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
+  const wrapped = shallow(<App />);
 
-  ReactDOM.render(<App />, div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-  // Looks inside the div
-  // and checks to see if the CommentBox is in there
-  expect(div.innerHTML).toContain('CommentBox');
+it('shows a comment list', () => {
+  const wrapped = shallow(<App />);
 
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
