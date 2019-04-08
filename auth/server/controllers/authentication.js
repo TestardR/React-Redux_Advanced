3a219ec81d8926fc4fda7ed2e5ef251a2eac9838
unknown <rmtestard@gmail.com>
2019-04-08 18:39:59 +0200

    creating a user Records

---
+++ auth/server/controllers/authentication.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+  const { email, password } = req.body;
+
+  // see if a user with a given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+    // if a suer with email does exist, return an error,
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    // if a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+    });
+
+    // respond to request indicating the user was created
+    res.json(user);
+  });
+};
