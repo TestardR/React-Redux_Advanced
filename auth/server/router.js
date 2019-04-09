cae155612d03030c3281810284bd906ae3df6e81
unknown <rmtestard@gmail.com>
2019-04-09 10:37:08 +0200

    auth route

--- auth/server/router.js
+++ auth/server/router.js
@@ -1,4 +1,9 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// We want to use jwt and not create a session cookie
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 // Model
 const User = require('./models/user');
@@ -8,6 +13,10 @@ module.exports = function(app) {
     User.find()
       .then(users => res.json(users))
       .catch(err => res.status(404).json({ nousersfound: 'No users found' }));
+  });
+
+  app.get('/auth', requireAuth, function(req, res) {
+    res.send({ hi: 'there' });
   });
 
   app.post('/signup', Authentication.signUp);
